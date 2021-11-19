c070a2e0580292104866b2c3035d1d81e51e26af
Jamie Hannah <jamiehannah1047@gmail.com>
2021-11-19 12:59:54 +0000

    State for dark mode added to nav button

--- quote-generator/src/App.js
+++ quote-generator/src/App.js
@@ -1,14 +1,26 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { useState } from 'react';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import MyQuotes from './components/MyQuotes';
 
 function App() {
+
+  const [toggleDark, setToggleDark] = useState(false);
+
+  function handleDarkMode() {
+    setToggleDark(!toggleDark);
+    console.log("Dark mode active? " + toggleDark)
+  }
+
   return (
     <Router>
       <div className="App">
-        <Navbar />
+        <Navbar 
+          handleDarkMode={handleDarkMode} 
+          toggleDark={toggleDark}
+        />
         <div>
           <Switch>
             <Route exact path="/">
--- quote-generator/src/components/Navbar.js
+++ quote-generator/src/components/Navbar.js
@@ -1,23 +1,20 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 import NavLogo from '../images/logo-black.png';
 
-const Navbar = () => {
+const Navbar = ({handleDarkMode, toggleDark}) => {
 
     const [toggleActive, setToggleActive] = useState(null);
 
-    useEffect(() => {
-        console.log(`Testing: The current state is ${toggleActive}`)
-    })
-
     return ( 
         <div className="navbar-container">
-            <nav className="navbar">
+            <nav className={ toggleDark ? "navbar-dark" : "navbar" }>
                 <img src={NavLogo} alt="" className="logo" />
                 <div className="links">
-                    <Link to="/" onClick={() =>setToggleActive("home")} className={toggleActive === "home" ? "navlink-active" : "navlink"}>Home</Link>
-                    <Link to="/my-quotes" onClick={() => setToggleActive("myQuotes")} className={toggleActive === "myQuotes" ? "navlink-active" : "navlink"}>My Quotes</Link>
+                    <Link to="/" onClick={() =>setToggleActive("home")} className={ toggleActive === "home" ? "navlink-active" : "navlink" }>Home</Link>
+                    <Link to="/my-quotes" onClick={() => setToggleActive("myQuotes")} className={ toggleActive === "myQuotes" ? "navlink-active" : "navlink" }>My Quotes</Link>
+                    <button className="toggle-button" onClick={handleDarkMode}>D/L</button>
                 </div>
             </nav>
         </div>
