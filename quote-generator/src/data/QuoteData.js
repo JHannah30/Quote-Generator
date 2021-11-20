99eb88042d802b2995d2007187fde80a1ffad296
Jamie Hannah <jamiehannah1047@gmail.com>
2021-11-20 14:38:49 +0000

    Finished adding quote data to object

--- quote-generator/src/data/QuoteData.js
+++ quote-generator/src/data/QuoteData.js
@@ -1,14 +1,31 @@
 // Importing images of Authors
-import NelsonMandela from '../images/quote-authors/nelson-mandela.png';
-import WaltDisney from '../images/quote-authors/walt-disney.png';
-import SteveJobs from '../images/quote-authors/steve-jobs.png';
-import EleanorRoosevelt from '../images/quote-authors/eleanor-roosevelt.png';
-import OprahWinfrey from '../images/quote-authors/oprah-winfrey.png';
-import JamesCameron from '../images/quote-authors/james-cameron.png';
-import JohnLennon from '../images/quote-authors/john-lennon.png';
-import MotherTheresa from '../images/quote-authors/mother-theresa.png';
-import FranklinDRoosevelt from '../images/quote-authors/franklin-d-roosevelt.png';
-import MargaretMead from '../images/quote-authors/margaret-mead.png';
+// import NelsonMandela from '../images/quote-authors/nelson-mandela.png';
+// import WaltDisney from '../images/quote-authors/walt-disney.png';
+// import SteveJobs from '../images/quote-authors/steve-jobs.png';
+// import EleanorRoosevelt from '../images/quote-authors/eleanor-roosevelt.png';
+// import OprahWinfrey from '../images/quote-authors/oprah-winfrey.png';
+// import JamesCameron from '../images/quote-authors/james-cameron.png';
+// import JohnLennon from '../images/quote-authors/john-lennon.png';
+// import MotherTheresa from '../images/quote-authors/mother-theresa.png';
+// import FranklinDRoosevelt from '../images/quote-authors/franklin-d-roosevelt.png';
+// import MargaretMead from '../images/quote-authors/margaret-mead.png';
+// import RobertLouisStevenson from '../images/quote-authors/robert-louis-stevenson.png';
+// import BenjaminFranklin from '../images/quote-authors/benjamin-franklin.png';
+// import HellenKeller from '../images/quote-authors/helen-keller.png';
+// import Aristotle from '../images/quote-authors/aristotle.png';
+// import AnneFrank from '../images/quote-authors/anne-frank.png';
+// import RalphWaldoEmerson from '../images/quote-authors/ralph-waldo-emerson.png';
+// import MayaAngelou from '../images/quote-authors/maya-angelou.png';
+// import AbrahamLincoln from '../images/quote-authors/abraham-lincoln.png';
+// import BabeRuth from '../images/quote-authors/babe-ruth.png';
+// import ThomasEdison from '../images/quote-authors/thomas-edison.png';
+// import DrSeuss from '../images/quote-authors/dr-seuss.png';
+// import WinstonChurchill from '../images/quote-authors/winston-churchill.png';
+// import HenryDavidThoreau from '../images/quote-authors/henry-david-thoreau.png';
+// import JohnDRockefellarJr from '../images/quote-authors/john-d-rockefellar-jr.png';
+// import ThomasJefferson from '../images/quote-authors/thomas-jefferson.png';
+// import AudreyHepburn from '../images/quote-authors/audrey-hepburn.png';
+// import WayneGretzky from '../images/quote-authors/WayneGretzky.png';
 
 
 
@@ -17,151 +34,152 @@ export const QuoteData = [
     {
         quote: "The greatest glory in living lies not in never falling, but in rising every time we fall",
         author: "Nelson Mandela",
-        image: NelsonMandela,
+        // image: NelsonMandela,
     },
     {
         quote: "The way to get started is to quit talking and begin doing",
         author: "Walt Disney",
-        image: WaltDisney,
+        // image: WaltDisney,
     },
     {
         quote: "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking",
         author: "Steve Jobs",
-        image: SteveJobs,
+        // image: SteveJobs,
     },
     {
         quote: "If life were predictable it would cease to be life, and be without flavor",
         author: "Eleanor Roosevelt",
-        image: EleanorRoosevelt,
+        // image: EleanorRoosevelt,
     },
     {
         quote: "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough",
         author: "Oprah Winfrey",
-        image: OprahWinfrey,
+        // image: OprahWinfrey,
     },
     {
         quote: "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success",
         author: "James Cameron",
-        image: JamesCameron,
+        // image: JamesCameron,
     },
     {
         quote: "Life is what happens when you're busy making other plans",
         author: "John Lennon",
-        image: JohnLennon,
+        // image: JohnLennon,
     },
     {
         quote: "Spread love everywhere you go. Let no one ever come to you without leaving happier",
         author: "Mother Theresa",
-        image: MotherTheresa,
+        // image: MotherTheresa,
     },
     {
         quote: "When you reach the end of your rope, tie a knot in it and hang on",
         author: "Franklin D. Roosevelt",
-        image: FranklinDRoosevelt,
+        // image: FranklinDRoosevelt,
     },
     {
         quote: "Always remember that you are absolutely unique. Just like everyone else",
         author: "Margaret Mead",
-        image: MargaretMead,
+        // image: MargaretMead,
+
     },
     {
         quote: "Don't judge each day by the harvest you reap but by the seeds that you plant",
         author: "Robert Louis Stevenson",
-        image: null,
+        // image: RobertLouisStevenson,
     },
     {
         quote: "The future belongs to those who believe in the beauty of their dreams",
         author: "Eleanor Roosevelt",
-        image: null,
+        // image: EleanorRoosevelt,
     },
     {
         quote: "Tell me and I forget. Teach me and I remember. Involve me and I learn",
         author: "Benjamin Franklin",
-        image: null,
+        // image: BenjaminFranklin,
     },
     {
         quote: "The best and most beautiful things in the world cannot be seen or even touched — they must be felt with the heart",
         author: "Helen Keller",
-        image: null,
+        // image: HelenKeller,
     },
     {
         quote: "It is during our darkest moments that we must focus to see the light",
         author: "Aristotle",
-        image: null,
+        // image: Aristotle,
     },
     {
         quote: "Whoever is happy will make others happy too",
         author: "Anne Frank",
-        image: null,
+        // image: AnneFrank,
     },
     {
         quote: "Do not go where the path may lead, go instead where there is no path and leave a trail",
         author: "Ralph Waldo Emerson",
-        image: null,
+        // image: RalphWaldoEmerson,
     },
     {
         quote: "You will face many defeats in life, but never let yourself be defeated",
         author: "Maya Angelou",
-        image: null,
+        // image: Maya Angelou,
     },
     {
         quote: "The greatest glory in living lies not in never falling, but in rising every time we fall",
         author: "Nelson Mandela",
-        image: null,
+        // image: NelsonMandela,
     },
     {
         quote: "In the end, it's not the years in your life that count. It's the life in your years",
         author: "Abraham Lincoln",
-        image: null,
+        // image: AbrahamLincoln,
     },
     {
         quote: "Never let the fear of striking out keep you from playing the game",
         author: "Babe Ruth",
-        image: null,
+        // image: BabeRuth,
     },
     {
         quote: "Life is either a daring adventure or nothing at all",
         author: "Helen Keller",
-        image: null,
+        // image: HelenKeller,
     },
     {
         quote: "Many of life's failures are people who did not realize how close they were to success when they gave up.",
         author: "Thomas A. Edison",
-        image: null,
+        // image: ThomasEdison,
     },
     {
         quote: "You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose",
         author: "Dr. Seuss",
-        image: null,
+        // image: DrSeuss,
     },
     {
         quote: "Success is not final; failure is not fatal: It is the courage to continue that counts",
         author: "Winston S. Churchill",
-        image: null,
+        // image: WinstonChurchill,
     },
     {
         quote: "Success usually comes to those who are too busy to be looking for it",
         author: "Henry David Thoreau",
-        image: null,
+        // image: HenryDavidThoreau,
     },
     {
         quote: "The secret of success is to do the common thing uncommonly well",
         author: "John D. Rockefeller Jr.",
-        image: null,
+        // image: JohnDRockefellarJr,
     },
     {
         quote: "I find that the harder I work, the more luck I seem to have",
         author: "Thomas Jefferson",
-        image: null,
+        // image: ThomasJefferson,
     },
     {
         quote: "Nothing is impossible, the word itself says, ‘I'm possible!'",
         author: "Audrey Hepburn",
-        image: null,
+        // image: AudreyHepburn,
     },
     {
         quote: "You miss 100% of the shots you don't take",
         author: "Wayne Gretzky",
-        image: null,
+        // image: WayneGretzky,
     },
 ]
