09c3b80bd494e9994f2c50116235fd8f51952458
Jamie Hannah <jamiehannah1047@gmail.com>
2021-11-21 22:14:49 +0000

    Added saved quotes tab as a component for cleaner code

--- quote-generator/src/components/MyQuotes.js
+++ quote-generator/src/components/MyQuotes.js
@@ -1,6 +1,7 @@
 import './MyQuotes.css';
 import { useState } from 'react';
 import QuoteCard from './QuoteCard';
+import SavedQuotesTab from './SavedQuotesTab';
 
 const MyQuotes = ({ 
     darkMode, 
@@ -27,29 +28,33 @@ const MyQuotes = ({
         setActiveTab("saved");
     }
 
+    const createNewQuote = () => {
+        console.log("Quote created... almost.")
+    }
+
     return ( 
         <div className={ darkMode ? "my-quotes-dark" : "my-quotes" }>
             <nav className="tab-bar">
+                
                 {/* Custom quotes button */}
                 <button className={ activeTab === "custom" ? "tab-btn left-tab active-tab" : "tab-btn left-tab" } onClick={showCustomQuotes}>Custom Quotes</button>
+                
                 {/* Saved quotes button */}
                 <button className={ activeTab === "saved" ? "tab-btn right-tab active-tab" : "tab-btn right-tab" } onClick={showSavedQuotes}>Saved Quotes</button>
+                
                 {/* Add new quote button */}
-                <button className="add-quote-btn">Add New Quote</button>
+                <button className="add-quote-btn" onClick={createNewQuote}>Add New Quote</button>
             </nav>
             <div className="quote-card-container">
+                
                 {/* Renders if Custom Quotes tab is active */}
                 {activeTab === "custom" && <div className="custom-quotes-body">
                     <h2 className="custom-quotes-message">It looks like you haven't made any of your own quotes yet.</h2>
-                    <h2 className="create-quote-link">Create one?</h2>
+                    <h2 className="create-quote-link" onClick={createNewQuote}>Create one?</h2>
                 </div>}
 
                 {/* Renders if Saved Quotes tab is active */}
-                {activeTab === "saved" && <div className="custom-body">
-                    <div className="quote-cards">
-                    {quoteCards}
-                    </div>
-                </div>}
+                {activeTab === "saved" && <SavedQuotesTab quoteCards={quoteCards}/>}
             </div>
         </div>
      );
---
+++ quote-generator/src/components/SavedQuotesTab.js
@@ -0,0 +1,9 @@
+const SavedQuotesTab = ({ quoteCards }) => {
+    return ( 
+        <div>
+                {quoteCards}
+        </div>
+     );
+}
+ 
+export default SavedQuotesTab;
