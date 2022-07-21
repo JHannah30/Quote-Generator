b93d543f7a5daca5d1c8f5eaea6628a2f12f016f
Jamie Hannah <jamiehannah1047@gmail.com>
2022-07-21 15:21:50 +0100

    Tidied up ToggleSpeechIcon.js

--- quote-generator/src/components/ToggleSpeechIcon.js
+++ quote-generator/src/components/ToggleSpeechIcon.js
@@ -13,19 +13,12 @@ const ToggleSpeechIcon = ({currentQuoteText}) => {
         setSpeechIcon(SelectedSpeech);
         window.speechSynthesis.speak(speechText);
         console.log(speechText)
-        
+
+        // Returning icon to untoggled when speech ends
         speechText.onend = function(e) {
             setSpeechIcon(UnselectedSpeech);
         };
     }
-
-    // // Changes speaker icon when it is clicked. The icon will revert back to it's original image after the setTimeout completes
-    // const toggleSpeechIcon = () => {
-    //     setSpeechIcon(SelectedSpeech);
-    //     setTimeout(() => {
-    //         setSpeechIcon(UnselectedSpeech);
-    //     }, 1000);
-    // }
 
     
     return ( 
